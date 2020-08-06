4858f915df305e09b23b43935e124733a06b2648
henry lee <djteski@gmail.com>
2020-08-06 15:06:00 -0500

    viruses moving randomly

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ var henryLocation = {
 var coronaLocation = [
     {
         top: 0,
-        left: 0
+        left: 100
     }, 
     {
         top: 0,
-        left: 0
+        left: 300
     }, 
     {
         top: 0,
-        left: 0
+        left: 500
     }
     ]
 
@@ -48,51 +48,27 @@ function locateCorona () {
 const startBtn = document.getElementById('btn-start')
 startBtn.addEventListener("click", startMoveCorona)
 
-// function startMoveCorona () {
-//   for (let i = 0; i < coronaLocation.length; i++) {
-//   if (coronaLocation[i].top > 10 && coronaLocation[i].top < 700) {
-//       coronaLocation[i].top++
-//   } else if (coronaLocation[i].left > 10 && coronaLocation[i].left < 1360) {
-//       coronaLocation[i].left++
-//   }
-//   console.log(coronaLocation[i].top)
-//   console.log(coronaLocation[i].left)
-// }
-// }
-// startMoveCorona()
-
 function startMoveCorona () {
-    // const randomTop = Math.floor(Math.random()*600)
-    // const randomLeft = Math.floor(Math.random()*1300)
-    // for (let i = 0; i < coronaLocation.length; i++) {
-    //     coronaLocation[i].top = randomTop
-    //     coronaLocation[i].left = randomLeft
-    //    }
-    setInterval(testCorona, 10)
-
-    function testCorona () {
+    setInterval(movingCorona, 1000)
+    function movingCorona () {
     const randomTop = Math.floor(Math.random()*600)
     const randomLeft = Math.floor(Math.random()*1300)
     for (let i = 0; i < coronaLocation.length; i++) {
         if (coronaLocation[i].top < 700 && coronaLocation[i].left < 1360) {
-            coronaLocation[i].top++
-            coronaLocation[i].left++
+            coronaLocation[i].top = randomTop
+            coronaLocation[i].left = randomLeft
         } else if (coronaLocation[i].top > 10 && coronaLocation[i].left > 10) {
             coronaLocation[i].top--
             coronaLocation[i].left--
         }
-        console.log(coronaLocation[i].top)
-        console.log(coronaLocation[i].left)
        }
     }
 }
 
 function gameLoop () {
     setTimeout(gameLoop, 1000)
-    // console.log(setTimeout)
     moveHenry()
     locateCorona()
-    // startMoveCorona()
 }
 
 gameLoop()
