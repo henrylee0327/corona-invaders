749da924d00aba5a655223058c6dbf9df5055905
henry lee <djteski@gmail.com>
2020-08-09 23:28:47 -0500

    Back to invadors

--- index.js
+++ index.js
@@ -3,6 +3,25 @@ var henryLocation = {
     left: 700
 }
 
+var coronaVirus = [
+    {top: 100, left: 100},
+    {top: 100, left: 250},
+    {top: 100, left: 400},
+    {top: 100, left: 550},
+    {top: 100, left: 700},
+    {top: 100, left: 850},
+    {top: 100, left: 1000},
+    {top: 300, left: 100},
+    {top: 300, left: 250},
+    {top: 300, left: 400},
+    {top: 300, left: 550},
+    {top: 300, left: 700},
+    {top: 300, left: 850},
+    {top: 300, left: 1000}
+]
+
+var missiles = []
+
 document.onkeydown = function (evt) {
     // console.log(evt)
     if (evt.keyCode === 38 && henryLocation.top > 10) {
@@ -13,6 +32,12 @@ document.onkeydown = function (evt) {
         henryLocation.left = henryLocation.left - 25
     } else if (evt.keyCode === 39 && henryLocation.left < 1360) {
         henryLocation.left = henryLocation.left + 25
+    } else if (evt.keyCode === 32) {
+        missiles.push({
+            left: henryLocation.left + 20,
+            top: henryLocation.top - 20
+        })
+        drawMissiles()
     }
     moveHenry()
 }
@@ -22,61 +47,25 @@ function moveHenry () {
     document.getElementById('henry').style.left = henryLocation.left + 'px'
 }
 
-const startBtn = document.getElementById('btn-start')
-startBtn.addEventListener("click", theGame)
-
-function theGame () {
-    const startGame = setInterval(moveCorona, 1300)
-    
-    function moveCorona () {
-    const theCorona = document.getElementById('corona')
-    const theCorona1 = document.getElementById('corona1')
-    const theCorona2 = document.getElementById('corona2')
-    const w = 1300, h = 600
-
-    theCorona.style.top = Math.floor(Math.random() * h) + 'px'
-    theCorona.style.left = Math.floor(Math.random() * w) + 'px'
-    theCorona1.style.top = Math.floor(Math.random() * h) + 'px'
-    theCorona1.style.left = Math.floor(Math.random() * w) + 'px'
-    theCorona2.style.top = Math.floor(Math.random() * h) + 'px'
-    theCorona2.style.left = Math.floor(Math.random() * w) + 'px'
-    
-}
-}
-
-function collisionDetect () {
-    var theHenry = henry.getBoundingClientRect();
-    var theCorona = corona.getBoundingClientRect();
-    
-    // if ((theCorona.top > theHenry.top && theCorona.top < theHenry.bottom) && (theCorona.bottom > theHenry.top && theCorona.bottom < theHenry.bottom)) {
-    //      verticalCollision = true
-    // } else {
-    //      verticalCollision = false
-    // }
-
-    // if ((theCorona.right > theHenry.left && theCorona.right < theHenry.right) && (theCorona.left < theHenry.right && theCorona.left > theHenry.left)) {
-    //      horizontalCollision = true
-    // } else {
-    //      horizontalCollision = false
-    // }
-
-    // if (verticalCollision && horizontalCollision) {
-    //     alert('collision detected')
-    // } else {
-    //     console.log('Game goes on')
-    // }
-    if (theCorona.top > theHenry.bottom || theCorona.right < theHenry.left || theCorona.bottom < theHenry.top || theCorona.left > theHenry.right) {
-        return false
-    } else {
-        alert('game over')
-        return true
+function drawCorona () {
+    document.getElementById('corona').innerHTML = ""
+    for (let i = 0; i < coronaVirus.length;i++) {
+        document.getElementById('corona').innerHTML += `<div class="corona1" style="top:${coronaVirus[i].top}px; left:${coronaVirus[i].left}px"></div>`
     }
-        
 }
-collisionDetect()
 
-// function gameLoop () {
-//     setTimeout(gameLoop, 1000)
-// }
+function drawMissiles () {
+    document.getElementById('missiles').innerHTML = ""
+    for (let i = 0; i < missiles.length; i++) {
+        document.getElementById('missiles').innerHTML += `<div class="missile" style=top:${missiles[i].top}px; left:${missiles[i].left}px></div>`
+    }
+}
 
-// gameLoop()
+
+function gameLoop () {
+    setTimeout(gameLoop, 1000)
+    drawCorona()
+    drawMissiles()
+}
+
+gameLoop()
