84a76ddcf6eb4ee38bf7be89ee5b97024d6e6542
henry lee <djteski@gmail.com>
2020-08-10 22:38:50 -0500

    Corona virus shooting complete

--- index.js
+++ index.js
@@ -11,16 +11,19 @@ var coronaVirus = [
     {top: 100, left: 700},
     {top: 100, left: 850},
     {top: 100, left: 1000},
+    {top: 100, left: 1150},
     {top: 300, left: 100},
     {top: 300, left: 250},
     {top: 300, left: 400},
     {top: 300, left: 550},
     {top: 300, left: 700},
     {top: 300, left: 850},
-    {top: 300, left: 1000}
+    {top: 300, left: 1000},
+    {top: 300, left: 1150}
 ]
 
 var missiles = []
+var coronaMissiles = []
 
 document.onkeydown = function (evt) {
     // console.log(evt)
@@ -41,7 +44,6 @@ document.onkeydown = function (evt) {
             left: henryLocation.left + 20,
             top: henryLocation.top - 40
         })
-        console.log(missiles)
     }
 }
 
@@ -57,6 +59,12 @@ function drawCorona () {
     }
 }
 
+function moveCorona () {
+    for (let i = 0; i < coronaVirus.length; i++) {
+        coronaVirus[i].top = coronaVirus[i].top + 5
+    }
+}
+
 function drawMissiles () {
     document.getElementById('missiles').innerHTML = ""
     for (let i = 0; i < missiles.length; i++) {
@@ -64,11 +72,75 @@ function drawMissiles () {
     }
 }
 
+function moveMissiles () {
+    for (let i = 0; i < missiles.length; i++) {
+        missiles[i].top = missiles[i].top - 20
+    }
+}
+
+setInterval(selectCoronaFiringMissiles, 2000)
+
+function selectCoronaFiringMissiles () {
+    var selectRandomCorona = coronaVirus[Math.floor(Math.random() * coronaVirus.length)]
+    console.log(selectRandomCorona)
+    coronaMissiles.push({
+        top: selectRandomCorona.top + 91,
+        left: selectRandomCorona.left + 45
+    })
+} selectCoronaFiringMissiles()
+
+function drawCoronaMissiles () {
+    document.getElementById('coronaMissiles').innerHTML = ""
+    for (let i = 0; i < coronaMissiles.length; i++) {
+        document.getElementById('coronaMissiles').innerHTML += `<div class="coronaMissile" style="left: ${coronaMissiles[i].left}px; top: ${coronaMissiles[i].top}px"></div>`
+    }
+}
+
+function moveCoronaMissiles () {
+    for (let i = 0; i < coronaMissiles.length; i++) {
+        coronaMissiles[i].top = coronaMissiles[i].top + 10
+    }
+}
+
+function collisionDetectMissile1 () {
+    for (let i = 0; i < coronaVirus.length; i++) {
+        for (let m = 0; m < missiles.length; m++) {
+            if (
+                (missiles[m].top <= coronaVirus[i].top + 91) &&
+                (missiles[m].top >= coronaVirus[i].top) &&
+                (missiles[m].left >= coronaVirus[i].left) &&
+                (missiles[m].left <= coronaVirus[i].left + 91)
+                ) {
+                coronaVirus.splice(i, 1)
+                missiles.splice(m, 1)
+            }
+        }
+    }
+}
+
+function collisionDetectMissile2 () {
+    for (let i = 0; i < coronaMissiles.length; i++) {
+        if (
+            (coronaMissiles[i].top + 20 >= henryLocation.top) &&
+            (coronaMissiles[i].top <= henryLocation.top) &&
+            (coronaMissiles[i].left >= henryLocation.left) &&
+            (coronaMissiles[i].left <= henryLocation.left + 50)
+        ) {
+            alert('Game over')
+        }
+    }
+}
+
 
 function gameLoop () {
-    setTimeout(gameLoop, 1000)
+    setTimeout(gameLoop, 300)
     drawCorona()
+    moveCorona()
     drawMissiles()
+    moveMissiles()
+    collisionDetectMissile1()
+    drawCoronaMissiles()
+    moveCoronaMissiles()
 }
 
 gameLoop()
